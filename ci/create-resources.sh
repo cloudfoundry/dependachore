ba1fa1a1702871d8aed0b97e657110b501c7a1c2
Kieron Browne <kbrowne@pivotal.io>
2019-11-13 16:44:24 +0000

    Auto-approve terraform apply changes
    [#168922532]
    
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>
    Co-authored-by: Mario Nitchev <marionitchev@gmail.com>

--- ci/create-resources.sh
+++ ci/create-resources.sh
@@ -6,5 +6,5 @@ echo "$GCP_SERVICE_ACCOUNT_JSON" > ./key
 export GOOGLE_APPLICATION_CREDENTIALS="$PWD/key"
 
 terraform init dependachore/ci/terraform
-terraform apply dependachore/ci/terraform
+terraform apply -auto-approve dependachore/ci/terraform
 
